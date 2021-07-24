92f10c1b3b3175c5b015511d1727a51408d4b84e
Priyam Gupta <priyamgupta@192.168.1.4>
2021-07-24 16:59:01 +0530

    first-commit

---
+++ src/main/java/com/priyam/transactionalredis/TransactionalRedisApplication.java
@@ -0,0 +1,13 @@
+package com.priyam.transactionalredis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionalRedisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionalRedisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/priyam/transactionalredis/TransactionalRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.priyam.transactionalredis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionalRedisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
